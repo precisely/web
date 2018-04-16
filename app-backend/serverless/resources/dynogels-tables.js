7b80ec592d4cfd30de0b14185b6a7059a56aadff
Aneil Mallavarapu <aneilmallavar@gmail.com>
2018-04-15 17:21:28 -0700

    Organize serverless/ folder

--- app-backend/serverless/resources/dynogels-tables.js
+++ app-backend/serverless/resources/dynogels-tables.js
@@ -1,12 +1,12 @@
 module.exports = () => {
   require('ts-node').register({module: "commonjs"});
   require('tsconfig-paths').register();
-  const {dynogels, tableNameWithoutStage} = require('../src/db/dynamo');
+  const {dynogels, tableNameWithoutStage} = require('src/db/dynamo');
   const {capitalize, camelCase} = require('lodash');
   const fs = require('fs');
 
   // force load models to populate dynogels.models
-  require('../src/services/models');
+  require('src/services/models');
   let params = {};
   const models = dynogels.models;
 
