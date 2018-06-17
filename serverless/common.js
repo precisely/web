4f6bbbe261352719e8a7f859ceccbbcc624ade7a
Aneil Mallavarapu <aneilmallavar@gmail.com>
2018-06-17 14:24:53 -0700

    Get backend serverless working with serverless/common.js
    Using a js file instead of .yml allows us to keep common configuration in
    a single place, and avoid a recursive evaluation loop problem that arose
    when attempting `${file(../serverless/config.yml)}`.
    
    This commit updates app-backend/serverless.yml only
    
    This deploys successfully.

---
+++ serverless/common.js
@@ -0,0 +1,34 @@
+module.exports.vars = (sls)=> {
+  const opt = sls.processedInput.options;
+  const env = process.env;
+
+  const stage = opt.stage || env.STAGE || 'dev';
+  const region = opt.region || env.REGION || 'us-east-1';
+
+  // Corresponds to an AWS account (dev, beta, prod)
+  const account = /^beta|prod|offine$/.test(stage) ? stage : 'dev';
+  const defaultProfile = `${account}-profile-precisely`;
+  const profile = opt.profile || env.PROFILE || defaultProfile;
+  const auth0Tenant = account==='offline' ? 'dev-precisely' : `${account}-precisely`;
+  const rootDomain = account==='prod' ? 'precise.ly'
+                   : account==='offline' ? 'localhost'
+                   : `codeprecisely.net`;
+  const baseDomain = account==='dev' ? `${stage}.${rootDomain}` : rootDomain;
+  const apiDomain = account==='offline' ? 'localhost' : `api.${baseDomain}`;
+  const certificateName = `*.${rootDomain}`;
+  const reactURL = account==='offline' ? `https://${baseDomain}:3000` : `https://${baseDomain}/`;
+  const graphQLAPIPath = '/graphql';
+
+  return {
+    account,
+    accountId: '#{AWS::AccountId}',
+    apiDomain,
+    auth0Tenant,
+    baseDomain,
+    certificateName,
+    graphQLAPIPath,
+    profile,
+    region,
+    stage
+  };
+};
