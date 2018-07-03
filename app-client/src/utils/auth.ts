619922262ce850f77efe195c4411534d5bb48b08
Constantine Vetoshev <vetoshev@gmail.com>
2018-07-02 19:09:16 -0700

    Improve the data we save in the authentication session.

--- app-client/src/utils/auth.ts
+++ app-client/src/utils/auth.ts
@@ -1,5 +1,10 @@
+import * as _ from 'lodash';
+import * as Auth0 from 'auth0-js';
+
+
 export const LS_AUTH_ACCESS_TOKEN = 'auth-access-token';
 export const LS_AUTH_EXPIRES_IN = 'auth-token-expires-at';
+export const LS_AUTH_USER = 'auth-user';
 
 
 export function logout() {
@@ -25,20 +30,27 @@ export function isAuthenticated() {
 }
 
 
-// FIXME:
-export function saveAuthentication(authResult: any, user: any) {
+export function saveAuthentication(authResult: Auth0.Auth0DecodedHash, user: object) {
   const expiresIn = (new Date().getTime() + authResult.expiresIn * 1000).toString();
   localStorage.setItem(LS_AUTH_ACCESS_TOKEN, authResult.accessToken);
   localStorage.setItem(LS_AUTH_EXPIRES_IN, expiresIn);
+  localStorage.setItem(LS_AUTH_USER, JSON.stringify(user));
 }
 
 
 export function removeAuthentication() {
   const keys = [
     LS_AUTH_ACCESS_TOKEN,
-    LS_AUTH_EXPIRES_IN
+    LS_AUTH_EXPIRES_IN,
+    LS_AUTH_USER
   ];
   for (const k of keys) {
     localStorage.removeItem(k);
   }
+}
+
+
+export function getUserName(): string {
+  const user = JSON.parse(localStorage.getItem(LS_AUTH_USER));
+  return _.get(user, ['name'], '');
 }
