75eb0bb85591c8b4ffd2eec10dab8bb00dd8813d
Constantine Vetoshev <vetoshev@gmail.com>
2018-07-04 20:03:02 -0700

    Add a simple test for Auth0 authorization behavior.

---
+++ app-client/src/__tests__/features/user/Login.test.tsx
@@ -0,0 +1,73 @@
+import * as Adapter from 'enzyme-adapter-react-16';
+import * as Auth0 from 'auth0-js';
+import * as AuthUtils from 'src/utils/auth';
+import * as Enzyme from 'enzyme';
+import * as React from 'react';
+import { Login } from 'src/features/user/Login';
+import { MemoryRouter } from 'react-router-dom';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('basic login component behavior', () => {
+
+  const envSaved = process.env;
+
+  beforeAll(() => {
+    process.env = { ...envSaved };
+    process.env['REACT_APP_AUTH0_CLIENT_ID'] = 'hello';
+  });
+
+  afterAll(() => {
+    process.env = envSaved;
+  });
+
+  // NB: This test sucks, as it does not exercise any of the tricky bits of
+  // login flow. A better test requires a full browser.
+
+  let spyAuthorize: any;
+  let spyParseHash: any;
+
+  beforeEach(() => {
+    spyAuthorize = jest.spyOn(Auth0.WebAuth.prototype, 'authorize').mockImplementation(() => {});
+    spyParseHash = jest.spyOn(Auth0.WebAuth.prototype, 'parseHash');
+  });
+
+  afterEach(() => {
+    spyParseHash.mockRestore();
+    spyAuthorize.mockRestore();
+  });
+
+  it('calls Auth0 Universal Login when there is no logged-in user', () => {
+    Enzyme.render(
+      <MemoryRouter>
+        <Login location={{}} />
+      </MemoryRouter>
+    );
+    expect(spyAuthorize).toHaveBeenCalled();
+    expect(spyParseHash).not.toHaveBeenCalled();
+  });
+
+  it('interprets the Auth0 hash URL call into the callback', () => {
+    Enzyme.render(
+      <MemoryRouter>
+        <Login location={{hash: '#access_token'}} />
+      </MemoryRouter>
+    );
+    expect(spyAuthorize).not.toHaveBeenCalled();
+    expect(spyParseHash).toHaveBeenCalled();
+  });
+
+  it('makes no Auth0 calls when there is a logged-in uiser', () => {
+    AuthUtils.saveAuthentication({expiresIn: 60, accessToken: 'one', idToken: 'two'}, {});
+    Enzyme.render(
+      <MemoryRouter>
+        <Login location={{}} />
+      </MemoryRouter>
+    );
+    expect(spyAuthorize).not.toHaveBeenCalled();
+    expect(spyParseHash).not.toHaveBeenCalled();
+  });
+
+});
