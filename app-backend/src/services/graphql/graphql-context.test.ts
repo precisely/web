55ec088e868032347050ffbb142ba8d61806a949
aneilbaboo <aneilmallavar@gmail.com>
2018-07-20 09:54:41 -0700

    Break out GraphQLContext test helpers for use by other tests

--- app-backend/src/services/graphql/graphql-context.test.ts
+++ app-backend/src/services/graphql/graphql-context.test.ts
@@ -1,12 +1,11 @@
 // tslint:disable:no-any
 // tslint:disable:no-empty
 
-import { GraphQLContext } from './graphql-context';
 import context from 'jest-plugin-context';
-import { APIGatewayEvent, APIGatewayEventRequestContext, Context as LambdaContext} from 'aws-lambda';
-import { AuthResponseContext } from 'aws-lambda';
 import { AccessControlPlus, IContext } from 'accesscontrol-plus';
 import { defineModel } from 'src/db/dynamo/dynogels';
+import { GraphQLContext } from './graphql-context';
+import { makeEvent, makeLambdaContext } from './test-helpers';
 
 describe('GraphQLContext', function () {
   context('roles', function () {
@@ -198,74 +197,3 @@ describe('GraphQLContext', function () {
   });
 });
 
-function makeEvent({
-  authorizer = { principalId: '', roles: ''},
-  headers = { Authorization: 'Bearer abcd' }
-}: {
-  authorizer?: AuthResponseContext,
-  headers?: { [name: string]: string }
-}): APIGatewayEvent {
-  const requestContext: APIGatewayEventRequestContext = {
-    accountId: '1234',
-    apiId: 'asdf',
-    authorizer: authorizer,
-    httpMethod: 'POST',
-    identity: {
-      accessKey: '',
-      accountId: '',
-      apiKey: '',
-      apiKeyId: '',
-      caller: '',
-      cognitoAuthenticationProvider: '',
-      cognitoAuthenticationType: '',
-      cognitoIdentityId: '',
-      cognitoIdentityPoolId: '',
-      sourceIp: '',
-      user: '',
-      userAgent: '',
-      userArn: ''
-    },
-    stage: '',
-    requestId: 'req123',
-    requestTimeEpoch: 123123123,
-    resourceId: 'resource123',
-    resourcePath: '/api',
-
-  };
-  const event: APIGatewayEvent = {
-    body: 'the body',
-    headers,
-    httpMethod: 'POST',
-    isBase64Encoded: false,
-    path: '/api',
-    stageVariables: {},
-    pathParameters: null,
-    queryStringParameters: null,
-    requestContext: requestContext,
-    resource: '/api'
-  };
-  return event;
-}
-
-function makeLambdaContext(): LambdaContext {
-  return {
-    callbackWaitsForEmptyEventLoop: false,
-    functionName: '',
-    functionVersion: '',
-    invokedFunctionArn: '',
-    memoryLimitInMB: 100,
-    awsRequestId: '',
-    logGroupName: '',
-    logStreamName: '',
-    // identity?: CognitoIdentity;
-    // clientContext?: ClientContext;
-
-    // Functions
-    getRemainingTimeInMillis(): number { return 0; },
-
-    done(error?: Error, result?: any): void { },
-    fail(error: Error | string): void {},
-    succeed(messageOrObject: any): void {},
-    // succeed(message: string, object: any): void {}
-  };
-}
