a1f245f075c51a87df2004f8d905be9cdb081705
aneilbaboo <aneilmallavar@gmail.com>
2018-07-25 15:38:46 -0700

    Fix stub tests

--- app-backend/src/services/variant-call/models.test.ts
+++ app-backend/src/services/variant-call/models.test.ts
@@ -2,15 +2,15 @@ import { VariantCall } from './models';
 
 describe('VariantCall', function () {
   describe('forUser', function () {
-    it('should retrieve an instance if the user has variant calls for those ref-indexes');
-    it("should retrieve an empty list if the user doesn't have variant calls for those ref-indexes");
-    it('should retrieve an instance for a user given VariantCallIndexes with rsIds');
-    it("should retrieve an empty list if the user doesn't have variant calls for those rsIds");
-    it('should retrieve a list containing variant calls for refIndexes and rsIds');
+    it.skip('should retrieve an instance if the user has variant calls for those ref-indexes', () => null);
+    it.skip("should retrieve an empty list if the user doesn't have variant calls for those ref-indexes", () => null);
+    it.skip('should retrieve an instance for a user given VariantCallIndexes with rsIds', () => null);
+    it.skip("should retrieve an empty list if the user doesn't have variant calls for those rsIds", () => null);
+    it.skip('should retrieve a list containing variant calls for refIndexes and rsIds', () => null);
 
   });
 
   describe('save', function () {
-    it('should set the variantId correctly from the save parameters');
+    it.skip('should set the variantId correctly from the save parameters', () => null);
   });
 });
