d7da77a4f016a005b364141b6700909fcaaeb3ec
aneilbaboo <aneilmallavar@gmail.com>
2018-07-26 10:32:25 -0700

    WIP

---
+++ app-backend/src/services/system/index.ts
@@ -0,0 +1,7 @@
+/*
+* Copyright (c) 2018-Present, Precise.ly, Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or
+* without modification, are not permitted.
+*/
