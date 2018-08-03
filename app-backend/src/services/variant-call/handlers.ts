2df098958c984130c7068c760bfb4e530ba379fe
aneilbaboo <aneilmallavar@gmail.com>
2018-08-03 12:43:30 -0700

    Add VariantCall batchCreate lambda API

---
+++ app-backend/src/services/variant-call/handlers.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017-Present, Precise.ly, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are not permitted.
+ */
+import {Handler, Context, Callback} from 'aws-lambda';
+
+import {makeLogger} from 'src/common/logger';
+
+import { VariantCallService } from './service';
+import { VariantCallAttributes } from './models';
+
+export const batchCreate: Handler = (
+  event: VariantCallAttributes[], context: Context, callback: Callback
+) => {
+  const log = makeLogger(context.awsRequestId);
+  log.info('VariantCallBatchCreate EVENT: %j\t\tCONTEXT: %j', event, context);
+  VariantCallService.addVariantCalls(event)
+    .then(result => callback(null, result))
+    .catch(error => callback(error));
+};
