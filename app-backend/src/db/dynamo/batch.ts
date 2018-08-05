097f781b6bfe28a1b4fe8ba8f491a268a7587dfe
aneilbaboo <aneilmallavar@gmail.com>
2018-08-05 15:47:17 -0700

    Add silly logging during batchCreate and batchUpdate

--- app-backend/src/db/dynamo/batch.ts
+++ app-backend/src/db/dynamo/batch.ts
@@ -2,6 +2,7 @@ import { Model, CreateItemOptions, UpdateItemOptions } from '@aneilbaboo/dynogel
 import batchPromises = require('batch-promises');
 
 import { dynamoDBDefaultBatchSize } from 'src/common/environment';
+import { log } from 'src/common/logger';
 
 export type BatchItem<T> = { data: T, error?: string };
 
@@ -12,9 +13,11 @@ export async function batchCreate<Attrs>( // tslint:disable-line no-any
     dynamoDBDefaultBatchSize, createArgList, 
     async (createArgs: Attrs) => {
       try {
+        log.silly('batchCreate creating %j', <any> createArgs); // tslint:disable-line no-any
         const result = await model.createAsync(createArgs, options);
         return { data: <Attrs> result.get() };
       } catch (e) {
+        log.silly('batchCreate failed %j error: %j', <any> createArgs, <any> e); // tslint:disable-line no-any
         return { data: createArgs, error: e.toString() || ''};
       }
     }
@@ -28,9 +31,11 @@ export async function batchUpdate<Attrs>( // tslint:disable-line no-any
     dynamoDBDefaultBatchSize, updateArgList, 
     async (updateAttrs: Attrs) => {
       try {
+        log.silly('batchUpdate updating %j', <any> updateAttrs); // tslint:disable-line no-any
         const result = await model.updateAsync(updateAttrs, options);
         return { data: <Attrs> result.get() };
       } catch (e) {
+        log.silly('batchUpdate failed %j error: %j', <any> updateAttrs, <any> e); // tslint:disable-line no-any
         return { data: updateAttrs, error: e.toString() || ''};
       }
     }
