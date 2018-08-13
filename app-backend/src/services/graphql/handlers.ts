726d493174f61277260e680f39d1f1b6b32cfd83
aneilbaboo <aneilmallavar@gmail.com>
2018-08-12 22:22:40 -0700

    Fix lambda function hang
    This fixes a regression due to adding a keep-alive connection for DynamoDB.
    This means the event loop never clears. This causes a problem in AWS because,
    by default, Lambda will timeout.
    
    Also added better logging and error handling.

--- app-backend/src/services/graphql/handlers.ts
+++ app-backend/src/services/graphql/handlers.ts
@@ -20,28 +20,32 @@ import { GraphQLContext } from './graphql-context';
 import preciselyTypeDefs from 'src/services/schema';
 
 export const apiHandler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  const log = makeLogger(context.awsRequestId);
-  const PreciselySchema = makeExecutableSchema({
-    typeDefs: preciselyTypeDefs,
-    resolvers,
-    logger: { log(message: string) { log.info(message); } }
-  });
+  try {
+    const log = makeLogger(context.awsRequestId);
+    const PreciselySchema = makeExecutableSchema({
+      typeDefs: preciselyTypeDefs,
+      resolvers,
+      logger: { log(message: string) { log.info(message); } }
+    });
 
-  const handler = graphqlLambda({
-    schema: PreciselySchema,
-    tracing: true,
-    rootValue: null,
-    context: new GraphQLContext(event, context)
-  });
-  log.silly('graphql.apiHandler EVENT: %j\t\tCONTEXT: %j', event, context);
-  withCORS(handler, event, context, (err, result) => {
-    if (err) {
-      log.error('graphql.apiHandler error', err);
-    } else {
-      log.info('graphql.apiHandler result %j', result);
-    }
-    callback(err, result);
-  });
+    const handler = graphqlLambda({
+      schema: PreciselySchema,
+      tracing: true,
+      rootValue: null,
+      context: new GraphQLContext(event, context)
+    });
+    log.silly('graphql.apiHandler EVENT: %j\t\tCONTEXT: %j', event, context);
+    withCORS(handler, event, context, (err, result) => {
+      if (err) {
+        log.error('graphql.apiHandler error', err);
+      } else {
+        log.info('graphql.apiHandler result %j', result);
+      }
+      callback(err, result);
+    });
+  } catch (e) {
+    callback(e);
+  }
 };
 
 export function playgroundTitle(stage?: string) {
@@ -51,22 +55,20 @@ export function playgroundTitle(stage?: string) {
 } 
 
 export const playgroundHandler: Handler = function (event: APIGatewayEvent, context: Context, callback: Callback) {
-  const handler = lambdaPlayground({
-    endpoint: process.env.GRAPHQL_API_PATH,
-    htmlTitle: playgroundTitle(process.env.STAGE)
-  });
-  withCORS(handler, event, context, callback);
+  try {
+    const handler = lambdaPlayground({
+      endpoint: process.env.GRAPHQL_API_PATH,
+      htmlTitle: playgroundTitle(process.env.STAGE)
+    });
+    withCORS(handler, event, context, callback);
+  } catch (e) {
+    callback(e);
+  }
 };
 
-// export const graphiqlHandler: Handler = function (event: APIGatewayEvent, context: Context, callback: Callback) {
-//   const handler = graphiqlLambda({ endpointURL: process.env.GRAPHQL_API_PATH });
-
-//   withCORS(handler, event, context, callback);
-// };
-
 function withCORS(handler: Handler, event: APIGatewayEvent, context: Context, callback: Callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
   const log = makeLogger(context.awsRequestId);
-  log.debug('APIGateway event: %j, context: %j', event, context);
   const callbackFilter = function (error: Error, output?: any ) {
     if (output) {
       output.headers = output.headers || {};
@@ -76,10 +78,12 @@ function withCORS(handler: Handler, event: APIGatewayEvent, context: Context, ca
         'Access-Control-Allow-Credentials' : true // Required for cookies, authorization headers with HTTPS
       });
     }
+    log.silly('%s complete error: %j, result: %j', handler.name, error, output);
     callback(error, output);
   };
 
   try {
+    log.debug('%s event: %j, context: %j', handler.name, event, context);
     return handler(event, context, callbackFilter);
   } catch (e) {
     return callbackFilter(e);
