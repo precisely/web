f3b52f770a9e071d23030e05712772b216b38641
aneilbaboo <aneilmallavar@gmail.com>
2018-08-16 14:16:54 -0700

    Calculate the end chromosome position of a variant correctly
    Deletion is currently represented as a `.` altBase.

--- app-backend/src/services/variant-call/models.test.ts
+++ app-backend/src/services/variant-call/models.test.ts
@@ -18,6 +18,127 @@ describe('VariantCall', function () {
   });
 
   describe('save', function () {
-    it.skip('should set the variantId correctly from the save parameters', () => null);
+    describe('a single nucleotide polymorphism', function () {
+      let vc: VariantCall;
+
+      beforeAll(async () => {
+        vc = await new VariantCall({
+          userId: 'bob-user-id',
+          refName: 'chr1',
+          refVersion: '37p13',
+          start: 100,
+          sampleType: '23andme',
+          sampleId: 'sampleId123',
+          genotype: [1, 1],
+          refBases: 'c',
+          altBases: ['t']
+        }).saveAsync();
+      });
+
+      afterAll(async () => {
+        await vc.destroyAsync();
+      });
+
+      it('should set the variantId correctly for a SNP', () => {
+        expect(vc.get('variantId')).toEqual('chr1:37p13:100:101:23andme:sampleId123');
+      });
+
+      it('should have properties set as expected', function () {
+        expect(vc.get('userId')).toEqual('bob-user-id');
+        expect(vc.get('refName')).toEqual('chr1');
+        expect(vc.get('refVersion')).toEqual('37p13');
+        expect(vc.get('sampleType')).toEqual('23andme');
+        expect(vc.get('sampleId')).toEqual('sampleId123');
+      });
+
+      it('should uppercase the ref and altbases', function () {
+        expect(vc.get('refBases')).toEqual('C');
+        expect(vc.get('altBases')).toEqual(['T']);
+      });
+    });
+
+    describe('a variant which includes an insertion', function () {
+      let vc: VariantCall;
+
+      beforeAll(async () => {
+        vc = await new VariantCall({
+          userId: 'bob-user-id',
+          refName: 'chr1',
+          refVersion: '37p13',
+          start: 100,
+          sampleType: '23andme',
+          sampleId: 'sampleId123',
+          genotype: [1, 1],
+          refBases: 'c',
+          altBases: ['tag', 't']
+        }).saveAsync();
+      });
+
+      afterAll(async () => {
+        await vc.destroyAsync();
+      });
+
+      it('should set the end chromosome position correctly', () => {
+        expect(vc.get('end')).toEqual(103);
+        expect(vc.get('variantId')).toEqual('chr1:37p13:100:103:23andme:sampleId123');
+      });
+
+    });
+
+    describe('a variant which contains altBase insertions unreferenced by the genotype', function () {
+      let vc: VariantCall;
+
+      beforeAll(async () => {
+        vc = await new VariantCall({
+          userId: 'bob-user-id',
+          refName: 'chr1',
+          refVersion: '37p13',
+          start: 100,
+          sampleType: '23andme',
+          sampleId: 'sampleId123',
+          genotype: [0, 0],  // NOTE: genotype is wildtype
+          refBases: 'c',
+          altBases: ['tag', 'tttttt'] // these altBases aren't actually referenced
+        }).saveAsync();
+      });
+
+      afterAll(async () => {
+        await vc.destroyAsync();
+      });
+
+      it('should set the end chromosome position correctly', () => {
+        expect(vc.get('end')).toEqual(101);
+        expect(vc.get('variantId')).toEqual('chr1:37p13:100:101:23andme:sampleId123');
+      });
+
+    });
+
+    describe('a variant which represents a deletion', function () {
+      let vc: VariantCall;
+
+      beforeAll(async () => {
+        vc = await new VariantCall({
+          userId: 'bob-user-id',
+          refName: 'chr1',
+          refVersion: '37p13',
+          start: 100,
+          sampleType: '23andme',
+          sampleId: 'sampleId123',
+          genotype: [1, 1],
+          refBases: 'c',
+          altBases: ['.']
+        }).saveAsync();
+      });
+
+      afterAll(async () => {
+        await vc.destroyAsync();
+      });
+
+      it('should set the end chromosome position correctly', () => {
+        expect(vc.get('end')).toEqual(100);
+        expect(vc.get('variantId')).toEqual('chr1:37p13:100:100:23andme:sampleId123');
+      });
+
+    });
   });
 });
