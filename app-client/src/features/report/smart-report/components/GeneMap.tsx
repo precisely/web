f2289b94c13add2b01628cce49f66943d376acd0
aneilbaboo <aneilmallavar@gmail.com>
2018-08-25 08:33:41 -0700

    Remove debugging code GeneMap
    And prettify code

--- app-client/src/features/report/smart-report/components/GeneMap.tsx
+++ app-client/src/features/report/smart-report/components/GeneMap.tsx
@@ -5,7 +5,6 @@ export const GeneMap: React.StatelessComponent<{
   start: number,
   end: number
 }> = ({start, end, children}) => {
-  const elt = <div><p>Variant at 50 showing A to T change</p></div>;
   return (
     <div>
       <p>GeneMap showing variants from {start} to {end} goes here</p>
@@ -19,6 +18,11 @@ export const Variant: React.StatelessComponent<{
   refBases: string,
   altBases: string
 }> = ({pos, refBases, altBases, children}: any) => {
-  return <div><p>Variant at {pos} showing {refBases} to {altBases} change</p>{children}</div>;
+  return (
+    <div>
+      <p>Variant at {pos} showing {refBases} to {altBases} change</p>
+      {children}
+    </div>
+  );
 };
 
