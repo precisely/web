a26c9b8d056b7b157d9a286cbef1cb9226e016aa
aneilbaboo <aneilmallavar@gmail.com>
2018-08-29 10:07:11 -0700

    Remove unnecessary code

--- app-client/config/env.js
+++ app-client/config/env.js
@@ -1,8 +1,5 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
-const paths = require('./paths');
 
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
