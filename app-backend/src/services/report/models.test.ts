72ac4daa1976a260aaee73985d27afa915c16512
aneilbaboo <aneilmallavar@gmail.com>
2018-09-10 22:14:36 -0700

    Add publishing to Report model

--- app-backend/src/services/report/models.test.ts
+++ app-backend/src/services/report/models.test.ts
@@ -26,7 +26,6 @@ describe('Report model', function () {
     it('should find an alternative slug when the slug exists', async () => {
       const report = new Report({
         title: 'foo',
-        content: ' ',
         ownerId: 'user123'
       });
       const savedReport = await report.saveAsync();
@@ -36,34 +35,30 @@ describe('Report model', function () {
     });
   });
 
-  describe('saving content with simple markdown', function () {
+  describe('a report with simple markdown', function () {
     let savedReport: Report;
 
     beforeAll(async function () {
       const report = new Report({
         title: 'Hello this is a new report',
-        content: '# This is a title',
+        content: '# This is the initial content',
         ownerId: 'user123'
       });
       savedReport = await report.saveAsync();
       rememberFixtures(savedReport);
     });
-
     afterAll(destroyFixtures);
 
     it('should be an instance of a report', () => {
       expect(savedReport).toBeInstanceOf(Report);
     });
 
-    it('should have a slug based on the title', () => {
-      expect(savedReport.get('slug')).toMatch(/^hello-this-is-a-new-report/);
+    it('should have content set to the initial content', () => {
+      expect(savedReport.getValid('content')).toEqual('# This is the initial content');
     });
 
-    it('should contain a parsed form of the markdown content', () => {
-      expect(savedReport.get('parsedContent')).toBeDefined();
-      expect(JSON.parse(<string> savedReport.get('parsedContent'))).toEqual([
-        { blocks: [ '<h1>This is a title</h1>'], type: 'text', reduced: false }
-      ]);
+    it('should have a slug based on the title', () => {
+      expect(savedReport.get('slug')).toMatch(/^hello-this-is-a-new-report/);
     });
 
     it('should not change the slug when updating an existing report', async () => {
@@ -72,20 +67,45 @@ describe('Report model', function () {
       expect(updatedReport.get('slug')).toMatch(/^hello-this-is-a-new-report/);
     });
 
-    it('should update the parsedContent when updating an existing report', async () => {
-      savedReport.set({content: '# This is an UPDATED title'});
-      const updatedReport = await savedReport.saveAsync();
-      
-      expect(JSON.parse(updatedReport.getValid('parsedContent'))).toEqual([
-        { blocks: [ '<h1>This is an UPDATED title</h1>'], type: 'text', reduced: false }
-      ]);
+    describe('when published', function () {
+      let publishedReport: Report;
+
+      beforeAll(async function () {
+        publishedReport = await savedReport.publish();
+      });
+
+      it('should contain the content as published content', () => {
+        expect(publishedReport.getValid('publishedContent')).toEqual('# This is the initial content');
+      });
+
+      it('should contain a parsed form of the markdown content in publishedElements', async () => {
+        expect(publishedReport.get('publishedElements')).toEqual([
+          { blocks: [ '<h1>This is the initial content</h1>'], type: 'text', reduced: false }
+        ]);
+      });
+
+      it('should update the publishedElements when publishing an existing report with new content', async () => {
+        savedReport.set({content: '# This is UPDATED content'});
+        const updatedReport = await savedReport.saveAsync();
+        
+        // before publishing - publishedElements is as before
+        expect(updatedReport.getValid('publishedElements')).toEqual([
+          { blocks: [ '<h1>This is the initial content</h1>'], type: 'text', reduced: false }
+        ]);
+
+        await updatedReport.publish();
+
+        expect(updatedReport.getValid('publishedElements')).toEqual([
+          { blocks: [ '<h1>This is UPDATED content</h1>'], type: 'text', reduced: false }
+        ]);
+      });
     });
   });
 
-  describe('saving content containing errors', function () {
+  describe('content containing errors', function () {
     afterEach(destroyFixtures);
 
-    it('should return an error', async function () {
+    it('should save successfully', async function () {
       const report = new Report({
         title: 'Report with error',
         content: 'First Line OK\n12345{<}This is text after the error',
@@ -93,13 +113,28 @@ describe('Report model', function () {
       });
 
       const savePromise = report.saveAsync();
-      await expect(savePromise).rejects.toBeInstanceOf(Error);
-      await expect(savePromise).rejects.toHaveProperty('errors.0.location.lineNumber', 2);
-      await expect(savePromise).rejects.toHaveProperty('errors.0.location.columnNumber', 7);
+      rememberFixtures(report);
+      await expect(savePromise).resolves.toBeInstanceOf(Report);
+    });
+
+    it('should throw an error when an attempt is made to publish', async () => {
+      const report = new Report({
+        title: 'Report with error',
+        content: 'First Line OK\n12345{<}This is text after the error',
+        ownerId: 'user123'
+      });
+
+      await report.saveAsync();
+      rememberFixtures(report);
+
+      const publishPromise = report.publish();
+      await expect(publishPromise).rejects.toBeInstanceOf(Error);
+      await expect(publishPromise).rejects.toHaveProperty('errors.0.location.lineNumber', 2);
+      await expect(publishPromise).rejects.toHaveProperty('errors.0.location.columnNumber', 7);
     });
   });
 
-  describe('saving content containing variants', function () {
+  describe('publishing content containing variants', function () {
     afterEach(destroyFixtures);
 
     it('should have variantIndexes set correctly', async function () {
@@ -114,7 +149,9 @@ describe('Report model', function () {
       const savedReport = await report.saveAsync();
       rememberFixtures(savedReport);
 
-      const variantIndexes = report.get('variantIndexes');
+      await savedReport.publish();
+
+      const variantIndexes = savedReport.get('variantIndexes');
       expect(variantIndexes).toBeDefined();
       expect(variantIndexes).toEqual([
         { refName: 'chr1', refVersion: '37p13', start: 10 },
