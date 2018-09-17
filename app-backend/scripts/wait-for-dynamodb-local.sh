a5922f9ff73422882d780e218525784f0fa2ee12
Aneil Mallavarapu <aneilmallavar@gmail.com>
2018-09-17 16:27:41 -0700

    Fix minor warnings and messages related to Dynamo

--- app-backend/scripts/wait-for-dynamodb-local.sh
+++ app-backend/scripts/wait-for-dynamodb-local.sh
@@ -15,17 +15,17 @@ max_attempts=30
 sleep_interval=1
 while [[ ! $(${cmd}) ]]; do
     if [[ ! $(ps auwx | grep DynamoDBLocal_lib | grep -v 'grep DynamoDBLocal_lib') ]]; then
-        echo "local DynamoDB not started" 1>&2
+        echo "Local DynamoDB not started" 1>&2
         exit 1
     fi
     (( attempt += 1 ))
     if [[ ${attempt} -gt ${max_attempts} ]]; then
-        echo "local DynamoDB startup failed after ${max_attempts} waits of ${sleep_interval}sec each" 1>&2
+        echo "Local DynamoDB startup failed after ${max_attempts} waits of ${sleep_interval}sec each" 1>&2
         exit 1
     fi
     echo "waiting for DynamoDB local... ${attempt}/${max_attempts}"
     sleep ${sleep_interval}
 done
 
-echo "local DynamoDB is running"
+echo "Local DynamoDB is running"
 exit 0
