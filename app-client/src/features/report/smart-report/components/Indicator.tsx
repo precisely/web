d7a14616ef88f5d14feb104d0950ebbe58c92999
Constantine Vetoshev <vetoshev@gmail.com>
2018-10-19 21:14:11 -0700

    Improve gene link styling.

--- app-client/src/features/report/smart-report/components/Indicator.tsx
+++ app-client/src/features/report/smart-report/components/Indicator.tsx
@@ -10,26 +10,15 @@
  * @Last Modified by: Aneil Mallavarapu
  * @Last Modified time: 2018-09-25 09:04:14
  */
+
+
 import * as React from 'react';
+import Radium from 'radium';
+
+import * as Styles from 'src/constants/styles';
+
 
 export type IndicatorProps = {icon: string, name: string, state: string, link: string, disabled: boolean};
-
-export const Indicator: React.StatelessComponent<IndicatorProps> = ({icon, name, state, link, disabled}: IndicatorProps) => {
-  const iconImage = disabled ? BUTTON[icon][state]['disabled'] : BUTTON[icon][state]['rest'];
-  // FIXME: need to incorporate hover, down, disabled states
-  const clickHandler = () => {
-    console.log('Navigating to %s', link);
-    window.location.href = link;
-  };
-  // FIXME: these aren't the right colors:
-  const fontColor = disabled ? 'lightgray' : 'black';
-  return (
-    <div style={{float: 'left', margin: '5px'}} onClick={disabled ? null : clickHandler}>
-      <div key="indicatorName" style={{color: fontColor}}>{name}</div>
-      <div key="indicatorImage"><img src={iconImage}/></div>
-    </div>
-  );
-};
 
 
 const BUTTON = {
@@ -51,6 +40,44 @@ const BUTTON = {
       hover: require('src/assets/indicator/dna/defective/hover.png'),
       down: require('src/assets/indicator/dna/defective/down.png'),
       disabled: require('src/assets/indicator/dna/defective/disabled.png')
+    }
+  }
+};
+
+
+export const Indicator: React.StatelessComponent<IndicatorProps> = Radium(({icon, name, state, link, disabled}: IndicatorProps) => {
+  const clickHandler = () => {
+    window.location.href = link;
+  };
+  // FIXME: these aren't the right colors:
+  const fontColor = disabled ? 'lightgray' : 'black';
+  return (
+    <div style={indicatorBoxStyle} onClick={disabled ? null : clickHandler}>
+      <div key="indicatorName" style={{color: fontColor}}>{name}</div>
+      <div key="indicatorImage" style={indicatorImageStyle(icon, state, disabled)} />
+    </div>
+  );
+});
+
+
+const indicatorBoxStyle: React.CSSProperties = {
+  float: 'left',
+  margin: '5px',
+  cursor: 'pointer'
+};
+
+const indicatorImageStyle = (icon: string, state: string, disabled: boolean): Styles.ExtendedCSSProperties => {
+  return {
+    backgroundImage: `url(${BUTTON[icon][state][disabled ? 'disabled' : 'rest']})`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '20px',
+    ':hover': {
+      backgroundImage: `url(${BUTTON[icon][state][disabled ? 'disabled' : 'hover']})`
+    },
+    ':active': {
+      backgroundImage: `url(${BUTTON[icon][state][disabled ? 'disabled' : 'down']})`
     }
   }
 };
