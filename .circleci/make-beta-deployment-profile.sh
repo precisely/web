e79c29367922cc7576c7ff756d43fe8b4de84c35
Constantine Vetoshev <vetoshev@gmail.com>
2020-01-12 19:25:48 -0800

    CircleCI configuration updates to build the production front end.

--- .circleci/make-beta-deployment-profile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-set -eo pipefail
-
-PROFILE="beta-profile-precisely"
-
-aws configure set aws_access_key_id "$BETA_AWS_ACCESS_KEY_ID" --profile $PROFILE
-aws configure set region $AWS_REGION --profile $PROFILE
-aws configure set aws_secret_access_key  "$BETA_AWS_SECRET_KEY" --profile $PROFILE
