df52441a1f878fc8a96408f7dab8e5eb4185bf85
Constantine Vetoshev <vetoshev@gmail.com>
2020-08-18 23:17:56 -0700

    Revert "Turn off CSS extraction plugin."
    This reverts commit c0058e8d8d3783be220ede03d345174e9911b46e.

--- app-client/config/webpack.config.prod.js
+++ app-client/config/webpack.config.prod.js
@@ -297,9 +297,9 @@ module.exports = {
       sourceMap: shouldUseSourceMap,
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    // new ExtractTextPlugin({
-    //   filename: cssFilename,
-    // }),
+    new ExtractTextPlugin({
+      filename: cssFilename,
+    }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
